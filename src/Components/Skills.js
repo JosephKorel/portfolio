8217d80615813e87f91cd0f103bb6e6a2937c5ba
José Carlos <jcjosecarlos03@gmail.com>
2022-05-18 15:16:35 -0300

    change skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -13,12 +13,18 @@ function Skills(props) {
           <img src="./css.png" alt="CSS" id="css"></img>
           <img src="./js.png" alt="JavaScript" id="js"></img>
           <img src="./react.png" alt="React" id="react"></img>
+          <img src="./git-icon.png" alt="Git" id="git-icon"></img>
+          <img src="./firebase.png" alt="Firebase" id="firebase"></img>
+          <img src="./tailwind.png" alt="Tailwind CSS" id="tailwind"></img>
+          <img src="./mui.png" alt="Material UI" id="mui"></img>
+          <img src="./chakra.png" alt="Chakra UI" id="chakra"></img>
         </div>
         <p>
           Estas são as tecnologias que eu mais possuo familiaridade, sendo React
-          meu principal objeto de estudo. Todos meus projetos até então foram
-          feitos utilizando <span className="react">React</span>. Além disso,
-          também estou estudando <span>Redux</span> e <span>Nodejs</span>.{" "}
+          o framework que utilizo atualmente. Todos meus projetos até então
+          foram feitos utilizando <span className="react">React</span>. Além
+          disso, utilizo Tailwind CSS e componentes das bibliotecas Material UI
+          e Chakra UI.
         </p>
         <button className="skill-return" onClick={() => Return()}>
           <img src="./return.png" alt="Voltar"></img>
